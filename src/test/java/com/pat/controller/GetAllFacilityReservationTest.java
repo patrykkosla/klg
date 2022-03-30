c872a47c024a76f46ae9bb351e626ba6a1eb881e
patryk <p.kosla@gmail.com>
2022-03-30 15:29:08 +0200

    poprawka do testu - json fail

--- src/test/java/com/pat/controller/GetAllFacilityReservationTest.java
+++ src/test/java/com/pat/controller/GetAllFacilityReservationTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.pat.model.Facility;
 import com.pat.model.Reservation;
+import com.pat.model.User;
 import com.pat.service.ReservationService;
 
 import static org.mockito.Mockito.when;
@@ -36,14 +38,24 @@ public class GetAllFacilityReservationTest {
     void shouldGetListofReservations() throws Exception {
     	List<Reservation> reservationsList = new ArrayList<>();
     	Reservation testReservation = new Reservation();
-    	testReservation.setId(22);
+    	testReservation.setId((long) 224444);
+    	Facility f = new Facility();
+    	f.setId((long) 234);
+    	User u = new User();
+    	u.setUserId((long) 435345);
+    	u.setUserName("test user name");
+    	testReservation.setFacility(f);
+    	testReservation.setReservedBy(u);
     	reservationsList.add(testReservation);
+    	
+    	
+//    	testReservation.setId(23);
+//    	reservationsList.add(testReservation);
     	when(reservationService.getAllFacilityReservations((long) 3)).thenReturn(reservationsList);
     	mockMvc.perform(get("/api/reservations/facility/3"))
-    		.andDo(print())
     		.andExpect(status().isOk())
-    		.andExpect(content().contentType("List"))
-    		.andExpect(content().json(reservationsList.toString()));
+   	        .andExpect(content().json("[{\"id\":224444,\"facility\":{\"id\":234,\"facilityName\":null,\"pricePerDay\":0.0,\"propertyArea\":0.0,\"descryption\":null,\"owner\":null},\"reservedBy\":{\"userId\":435345,\"userName\":\"test user name\"},\"reservedFrom\":null,\"reservedTo\":null,\"reservationCost\":0.0}]")
+    				);
     }
     
 }
